d510c90d030c98924cf9b8817ba491cccc04a6fd
Igor Zelenyi <igor.zeleniy@onseo.biz>
2026-03-06 15:49:04 +0200

    clean code

--- cmd/mytets/mytets.go
+++ cmd/mytets/mytets.go
@@ -2,23 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
 )
 
-var mytetsCmd = &cobra.Command{
-	Use:   "mytets",
-	Short: "A brief description of your command",
-	Long:  `A longer description of your command.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("mytets called")
-	},
-}
-
 func main() {
-	if err := mytetsCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	fmt.Println("Тому шо треба жити інтєрєсно!")
 }
