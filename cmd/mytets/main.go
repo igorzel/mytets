ddc33a63524eb24ee5281ba930162ef371f3e1dc
Igor Zelenyi <igor.zeleniy@onseo.biz>
2026-04-17 15:01:08 +0300

    empty go project initiated (go mod init)

---
+++ cmd/mytets/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Тому шо треба не всякой х*йньой заніматься, а жити інтєрєсно!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/igorzel/mytets
+
+go 1.26.2
